b3594c70e801950bd49fea0cd24f65a36427f12f
wooooozin <wooooozin@gmail.com>
2023-04-03 22:37:23 +0900

    변수와 계산 - 변수

---
+++ Part1/Chapter01/src/ch01/VariableTest.java
@@ -0,0 +1,19 @@
+package ch01;
+
+public class VariableTest {
+	
+	public static void main(String[] args) {
+		
+		
+		// 변수란  - 값을 저장할 수 있는 메모리의 공간을 의미한다.
+		int count; // 변수의 선언, type 변수명;
+		count = 10;
+		count = 20;
+		
+//		count = 1.1; // 같은 타입만 가능;
+		
+		System.out.println(count);
+		
+	}
+
+}
