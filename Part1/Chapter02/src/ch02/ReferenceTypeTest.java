a9f8778757a1671bff9d4df1204fee9ef9e351a5
wooooozin <wooooozin@gmail.com>
2023-04-05 06:04:01 +0900

    참조타입

---
+++ Part1/Chapter02/src/ch02/ReferenceTypeTest.java
@@ -0,0 +1,19 @@
+package ch02;
+
+public class ReferenceTypeTest {
+
+	public static void main(String[] args) {
+
+		
+		// 참조타입 - 	참조형 타입은 기본형 타입을 제외한 모든 타입입니다. 앞서 배웠던, 배열도 참조형이고, 클래스도 모두 참조 타입이다
+		
+		// 기본타입 
+		int num = 4;
+		
+		// 참조타입
+		String string = new String("Hello");
+		System.out.println(string);
+		
+	}
+
+}
