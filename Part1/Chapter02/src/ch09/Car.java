3bd29c22d5010477734b8af445c637217c83299e
wooooozin <wooooozin@gmail.com>
2023-04-06 06:35:24 +0900

    생성자와 오버로딩

--- Part1/Chapter02/src/ch09/Car.java
+++ Part1/Chapter02/src/ch09/Car.java
@@ -20,7 +20,17 @@ public class Car {
 	
 	// this는 현재 객체, 자기 자신을 나타낸다.
 	// 클래스 안에서 자기 자신이 가지고 있는 메소드를 사용할 때도 this.메소드명()으로 호출할 수 있다.
-
+	
+	// 오버로딩 - 매개변수의 유형과 개수가 다르게 하여 같은 이름의 메소드를 여러 개 가질 수 있게하는 기술
+	// 메소드 오버로딩은 매개변수 부분이 달라야 한다.
+	public String showInfo(String name) {
+		return this.name;
+	}
+	
+	public String showInfo(String name, int number) {
+		return this.name + ", " + number;
+	}
+	
 	public void showInfo() {
 		System.out.println(this.name + "자동차 입니다.");
 	}
