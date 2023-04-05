19329b01f6c7dfa2e038754b6c7f199ed2097a8a
wooooozin <wooooozin@gmail.com>
2023-04-06 06:40:17 +0900

    상속

---
+++ Part1/Chapter02/src/ch10/BusTest.java
@@ -0,0 +1,13 @@
+package ch10;
+
+public class BusTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Bus bus1 = new Bus();
+		bus1.run();
+
+	}
+
+}
---
+++ Part1/Chapter02/src/ch10/Car.java
@@ -0,0 +1,9 @@
+package ch10;
+
+public class Car {
+	
+	public void run() {
+		System.out.println("차가 달립니다.");
+	}
+
+}
