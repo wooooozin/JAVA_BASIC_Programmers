574e7309063a004802b9c7467b3df393ad9a9298
wooooozin <wooooozin@gmail.com>
2023-04-06 22:39:28 +0900

    Exception

--- Part1/Chapter03/src/ch01/ExceptionTest.java
+++ Part1/Chapter03/src/ch01/ExceptionTest.java
@@ -31,6 +31,14 @@ public class ExceptionTest {
 			System.out.println("무조건 실행되는 finally 블럭");
 		}
 		
+		// Exception 처리하지 않았을때는 프로그램이 강제 종료되었는데 catch라는 블록으로 Exception을 처리하니 강제종료되지 않고 잘 실행되는 것을 알 수 있다.
+		// try블록에서 여러종류의 Exception이 발생한다면 catch라는 블록을 여러개 둘 수 있다.
+		// Exception클래스들은 모두 Exception클래스를 상속받으므로, 
+		// 예외클래스에 Exception을 두게 되면 어떤 오류가 발생하든지 간에 하나의 catch블록에서 모든 오류를 처리할 수 있다.
+
+
+
+		
 	}
 
 }
